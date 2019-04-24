5f48a1c25c5e02a369a79fdd07ead0e4d3ac0b59
Renat Khusaionv <sokolovskyinprison@list.ru>
2019-04-24 19:05:39 +0500

    renat

--- revizor/app/routes.py
+++ revizor/app/routes.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
+from flask import render_template
 from app import app
-from app.
 
 @app.route('/')
 @app.route('/main')
 def main():
-    return 
+    comments = [
+        {
+            'author': 'Игорь',
+            'body': 'отличный ресторан, клёвый я бы сказал'
+        },
+        {
+            'author': 'Ислам',
+            'body': 'хороший'
+        }
+    ]
+    return render_template('index.html', title = 'Ревизор', comments = comments)
